a09c54e2b77cead1c61e026ef5660c6aa25b3506
Allan Lenny <owinoallan77@gmail.com>
2023-02-15 23:42:29 +0300

    css

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -24,7 +24,7 @@ window.addEventListener('resize', showButton);
     <div className='navbar'>
       <div className='navbar-container'>
         <Link  to="/" className='navbar-logo'>
-          TRVL <i className='fab fa-typo3' />
+          ALLASHE <i className='fab fa-typo3' />
         </Link>
         <div className='menu-icon' onClick={handleClick}>
           <i className={click ? 'fas fa-times' : 'fas fa-bars' } />
